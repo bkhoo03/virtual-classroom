adc0177734c23a0b9a37c98f03c10ec47c8579d7
Sanjan Krishna Sarat <sanjankrishna007@protonmail.com>
2025-12-01 15:11:05 +0800

    Add ngrok-skip-browser-warning to CORS allowed headers

--- virtual-classroom/backend/src/server.ts
+++ virtual-classroom/backend/src/server.ts
@@ -69,7 +69,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'],
   exposedHeaders: ['Content-Range', 'X-Content-Range']
 }));
 app.use(express.json());
